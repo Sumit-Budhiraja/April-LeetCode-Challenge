848dd7ce74f36234c6dbfc0a2821f405e64cc095
Sumit Budhiraja <48406445+Sumit-Budhiraja@users.noreply.github.com>
2021-04-15 01:02:08 +0530

    Add files via upload

---
+++ Partition List.java
@@ -0,0 +1,32 @@
+class Solution {
+
+    // TC : O(n)
+    // SC  : O(1)
+    public ListNode partition(ListNode head, int x) {
+        ListNode smallerHead = new ListNode(-1);
+        ListNode greaterHead = new ListNode(-1);
+        ListNode sIt = smallerHead;
+        ListNode gIt = greaterHead;
+
+        ListNode it = head;
+        while(it!=null){
+            if(it.val <x){
+                // for smaller values
+                sIt.next = it;
+                it = it.next;
+                sIt =sIt.next;
+            } else{
+                // for equal or greater comparisons
+                gIt.next = it;
+                it = it.next;
+                gIt = gIt.next;
+            }
+        }
+
+        sIt.next = greaterHead.next;
+        gIt.next = null;
+
+        return smallerHead.next;
+
+    }
+}
