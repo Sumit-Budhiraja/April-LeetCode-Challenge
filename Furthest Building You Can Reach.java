eb9e4bcbb347a2b4032f51dad2195afbd9e3016c
Sumit Budhiraja <48406445+Sumit-Budhiraja@users.noreply.github.com>
2021-04-26 23:33:03 +0530

    Add files via upload

---
+++ Furthest Building You Can Reach.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int furthestBuilding(int[] heights, int bricks, int ladders) {
+       PriorityQueue<Integer> queue = new PriorityQueue<>();
+        for(int i=0;i<heights.length-1;i++)
+        {
+            int diff = heights[i+1]-heights[i];
+            if(diff>0){
+                queue.add(diff);
+                if(queue.size()>ladders)
+                {
+                    bricks-=queue.poll();
+                    if(bricks<0) return i;
+                }
+            }
+        }
+        return heights.length-1;
+    }
+}
